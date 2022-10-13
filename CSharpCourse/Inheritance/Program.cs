e2c4a03e055685c8d7b313b14cf909e422e7d2c2
BARIŞ KURT <kurt-bar07@hotmail.com>
2022-10-13 12:55:04 +0300

    RecapDemo2 finished

---
+++ CSharpCourse/Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{ FirstName="Barış"},
+                new Student{FirstName="Güldane"},
+                new Person{FirstName="Beyza"}
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+}
---
+++ CSharpCourse/VirtualMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            Console.WriteLine("-------");
+            Mysql mysql = new Mysql();
+            mysql.Add();
+            Console.ReadLine();
+        }
+    }
+    class Database
+    {
+        public virtual void Add() // inheritance edildiğinde içeriği degiştirilmek istenirse virtual kullanılması gerekli
+        {
+            Console.WriteLine("Added by default");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+    class SqlServer : Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by Sql Server");
+            base.Add(); // database in add ini kullanır
+        }
+    }
+    class Mysql : Database
+    {
+
+    }
+}
