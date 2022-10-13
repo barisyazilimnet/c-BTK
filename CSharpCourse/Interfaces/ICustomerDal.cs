4e51874a23744c8b8edb8417a82cd04f8b5b9e9d
BARIŞ KURT <kurt-bar07@hotmail.com>
2022-10-13 10:09:35 +0300

    Interface finished

---
+++ CSharpCourse/Interfaces/ICustomerDal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Server Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Server Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Server Updated");
+        }
+    }
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated");
+        }
+    }
+    class MysqlCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Mysql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Mysql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Mysql Updated");
+        }
+    }
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
---
+++ CSharpCourse/RecapDemo1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8]; // buttonlar için dizi nesnesi oluşturur
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button(); // her buton ayrı nesnedir
+                    buttons[i, j].Width = 100; // butonun genişliği
+                    buttons[i, j].Height = 100; // butonun yüksekliği
+                    buttons[i, j].Left = left; // butonun soldan konumu
+                    buttons[i, j].Top = top; // butonun yukarıdan konumu
+                    left += 100; // her buton eklendikten sonra 100 px saga kaydıracak
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i,j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i,j].BackColor = Color.White;
+                    }
+                    this.Controls.Add(buttons[i, j]);
+                }
+                left = 0; // saga kaydırmaı sıfırlamak lazım çünkü artık bir satır aşağı kayacak
+                top += 100; // her bir satır sonrasında 100 px aşağı kaydıracak
+            }
+        }
+    }
+}
