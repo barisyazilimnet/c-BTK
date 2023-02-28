5f743118afc4bf05851b91896a7c2e13da58b28b
BARIŞ KURT <kurt-bar07@hotmail.com>
2023-02-28 23:55:26 +0300

    28.02.2023

---
+++ MemberRegistration/MemberRegistration.ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemberRegistration.Business.Abstract;
+using MemberRegistration.Business.Concrete;
+using MemberRegistration.Business.DependencyResolvers.Ninject;
+using MemberRegistration.Entities.Concrete;
+
+namespace MemberRegistration.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var memberService = InstanceFactory.GetInstance<IMemberService>();
+            memberService.Add(new Member{Email = "kurt-bar07@hotmail.com", FirstName = "Barış", LastName = "KURT", DateOfBirth = new DateTime(2000,01,30), TcNo = "21487287098"});
+            Console.WriteLine("Eklendi");
+            Console.ReadLine();
+        }
+    }
+}
